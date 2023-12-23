053b6ec1ae583beeb91f3bd3a8a56145776e23e7
Jill <jho3292@gmail.com>
2023-12-22 20:01:10 -0500

    Add wavy text animation to status.

--- src/Status.jsx
+++ src/Status.jsx
@@ -1,11 +1,14 @@
-import { useState } from 'react'
+import './assets/Status.css'
 
 export default function Status(props) {
     const len = props.failedGuesses.length
 
+
     return(
         <div className="status-bar">
-            <p>{props.status}</p>
+            <p className="wavy-text">
+                {props.status.split('').map((letter, index) => <span style={{'--i':index + 1}}>{letter}</span>)}
+            </p>
             {len > 0 && 'Failed guesses: ' + props.failedGuesses.join(' ')}
         </div>
     );
