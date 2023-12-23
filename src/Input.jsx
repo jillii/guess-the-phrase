083c3b4979a5e04cb26270babf251481481f39a1
Jill <jho3292@gmail.com>
2023-12-23 10:50:42 -0500

    Make letter selection a dropdown.

--- src/Input.jsx
+++ src/Input.jsx
@@ -1,22 +1,29 @@
-import { useRef, useState } from "react";
+import { useRef, useState } from "react"
+import './assets/Input.css'
 
 export default function Input(props) {
-    const inputRef = useRef();
+    const inputRef = useRef()
+    const [possibleGuesses, setPossibleGuesses] = useState(['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'])
+
 
     const handleSubmit = (e) => {
         e.preventDefault();
-        props.onGuess(inputRef.current.value.toUpperCase())   
+        const input = inputRef.current.value.toUpperCase()
+
+        props.onGuess(input)
+        setPossibleGuesses(possibleGuesses.filter(x => x !== input))
+        // reset select
+        inputRef.current.value = 'Select'
     }
 
     return (
-        <form onSubmit={handleSubmit}>
-            <input 
-                type="text"
-                ref={inputRef}
-                placeholder="Make a guess"
-                maxLength="1"
-            />
-            <button type="submit">Enter</button>
-        </form>
+        <div className="select">
+            <select className="style widthHeight" onChange={handleSubmit} ref={inputRef}>
+                <option value="select" style={{pointerEvents: 'none'}}>Select</option>
+                {possibleGuesses.map((letter, index) =>
+                    <option key={index} value={letter}>{letter}</option>
+                )}
+            </select>
+        </div>
     );
 }
