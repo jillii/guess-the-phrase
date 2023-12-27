32908dec268b9e7b7ea127b4197da0978e91cb6a
Jill <jho3292@gmail.com>
2023-12-27 12:23:27 -0500

    Separate out words.

--- src/Board.jsx
+++ src/Board.jsx
@@ -4,20 +4,30 @@ import './assets/Board.css'
 export default function Board(props) {
     const answer = props.answer
     const guess = props.guess
+    const currentBoard = props.board
     let status = 0
     let delay = 0
+    let count = -2
     // handle lose
     if (props.mistakes <= 0) {
         status = -1
     }
     // handle win
-    if (JSON.stringify(props.board) == JSON.stringify(props.answer)) {
+    if (JSON.stringify(currentBoard) == JSON.stringify(props.answer)) {
         status = 1
     }
 
-    const board = props.board.map((letter, index) => {
-        if (guess === answer[index]) {delay = delay + .5}
-        return <Box key={index} value={letter} answer={answer[index]} isSpace={answer[index] == ' '} delay={guess === answer[index] ? delay : '0'} status={status} />
+    const board = answer.join('').split(' ').map((word, index) => {
+        count++
+        return (
+            <div className='word' key={index}>
+                {word.split('').map((letter, index) => {
+                    if (guess === answer[index]) {delay = delay + .5}
+                    {count = count + 1}
+                    return <Box key={index} value={currentBoard[count]} answer={answer[count]} delay={guess === answer[count] ? delay : '0'} status={status} />
+                })}
+            </div>
+        )
     })
 
     return (
--- src/Box.jsx
+++ src/Box.jsx
@@ -2,13 +2,12 @@ import './assets/Box.css'
 
 export default function Box(props) {
     const status = props.status
-    const isSpace = props.isSpace
-    const isMiss = status < 0 && !isSpace && props.value === ' '
+    const isMiss = status < 0 && props.value === ' '
     const value = isMiss ? props.answer : props.value
     const delay = props.delay
 
     return (
-        <div className={`box${isSpace ? ' is-space' : ''}${isMiss ? ' is-miss' : ''}${status == 1 && !isSpace ? ' is-win' : ''}`}>
+        <div className={`box${isMiss ? ' is-miss' : ''}${status == 1 ? ' is-win' : ''}`}>
             {value !== ' ' && <p style={{animationDelay: `${delay}s`}}>{value}</p>}
         </div>
     )
